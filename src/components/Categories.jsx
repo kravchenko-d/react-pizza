1d5bcf448a8f40b9f088337d053d1900502efcfd
kravchenko-d <kravchenko4@mail.ru>
2024-08-15 18:18:28 +0300

    init commit

---
+++ src/components/Categories.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react';
+
+function Categories() {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((category, id) => (
+          <li onClick={() => setActiveIndex(id)} className={activeIndex === id ? 'active' : ''}>
+            {category}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
