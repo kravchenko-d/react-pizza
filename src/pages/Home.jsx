88ae4566f6c1916203b8a1fea20bef1d9112d413
kravchenko-d <kravchenko4@mail.ru>
2024-08-16 20:04:30 +0300

    cart component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,8 @@
-import { useEffect, useState } from "react";
-import Categories from "../components/Categories";
-import PizzaBlock from "../components/PizzaBlock";
-import Skeleton from "../components/PizzaBlock/Skeleton";
-import Sort from "../components/Sort";
-
+import { useEffect, useState } from 'react';
+import Categories from '../components/Categories';
+import PizzaBlock from '../components/PizzaBlock';
+import Skeleton from '../components/PizzaBlock/Skeleton';
+import Sort from '../components/Sort';
 
 export const Home = () => {
   const [items, setItems] = useState([]);
@@ -16,10 +15,11 @@ export const Home = () => {
         setItems(json);
         setIsLoading(false);
       });
+      window.scrollTo(0, 0);
   }, []);
 
   return (
-    <>
+    <div className="container">
       <div className="content__top">
         <Categories />
         <Sort />
@@ -30,6 +30,6 @@ export const Home = () => {
           ? [...Array(6).fill(null)].map((_, index) => <Skeleton key={index} />)
           : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
       </div>
-    </>
+    </div>
   );
 };
