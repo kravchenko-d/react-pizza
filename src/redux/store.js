e45d41107e80832f56244dc21489e59abdfc3ceb
kravchenko-d <kravchenko4@mail.ru>
2024-08-23 18:29:28 +0300

    adding to cart

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import filterReducer from '../redux/slices/filterSlice';
+import cart from '../redux/slices/cartSlice';
 
 export const store = configureStore({
   reducer: {
     filter: filterReducer,
+    cart,
   },
 });
